cd0a133b0896e903246bbf78131f4222248b98ec
Aniket Mukherjee <aniketmukherjee1509@gmail.com>
2022-12-14 00:04:46 +0530

    authentication system completed

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.auth, name="auth_page"),
-    path("signup/", views.signin, name="signin"),
-    path("signin/", views.signup, name="signup"),
+    path("signin/", views.signin, name="signin"),
+    path("signup/", views.signup, name="signup"),
 ]
--- static/js/auth_page.js
+++ static/js/auth_page.js
@@ -1,6 +1,8 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const signinSaveBtn = document.getElementById('signin-btn');
+const signupSaveBtn = document.getElementById('signup-btn');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -8,4 +10,58 @@ signUpButton.addEventListener('click', () => {
 
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
+});
+
+signupSaveBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	const username = document.getElementById('signup-username').value;
+	const password = document.getElementById('signup-password').value;
+	const email = document.getElementById('signup-email').value;
+
+	let payload = {
+		method:'POST',
+		headers:{
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			'username': username,
+			'email': email,
+			'password': password
+		})
+	};
+
+	fetch('signup/', payload)
+	.then(response => response.json())
+	.then(res => {
+		if(!res.error){
+			console.log(res);
+			container.classList.remove("right-panel-active");		
+		}
+	})
+});
+
+signinSaveBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	const username = document.getElementById('signin-username').value;
+	const password = document.getElementById('signin-password').value;
+
+	let payload = {
+		method:'POST',
+		headers:{
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			'username': username,
+			'password': password
+		})
+	};
+
+	fetch('signin/', payload)
+	.then(response => response.json())
+	.then(res => {
+		if(!res.error){
+			console.log(res);
+			container.classList.remove("right-panel-active");		
+		}
+	})
 });
